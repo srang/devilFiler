c28df2f333de2af7dd60890a78ad511d9ed55ed9
Brad Levergood <brad.levergood@gmail.com>
2013-12-05 22:31:25 -0500

    inode class

---
+++ src/common/Inode.java
@@ -0,0 +1,48 @@
+package common;
+
+import java.util.ArrayList;
+
+public class Inode {
+	private int blockID;
+	private int offset;
+	private DFileID fileID;
+	private ArrayList<Integer> blockMap;
+	
+	// set methods
+	public void setBlockID(int block){
+		this.blockID = block;
+	}
+	
+	public void setOffset(int off){
+		this.offset = off;
+	}
+	
+	public void setfileID(DFileID ID){
+		this.fileID = ID;
+	}
+	
+	public void add(int fileBlock){
+		this.blockMap.add(fileBlock);
+	}
+	
+	// get methods
+	public int Size(){
+		return Constants.INODE_SIZE;
+	}
+	
+	public int getBlockID(){
+		return this.blockID;
+	}
+	
+	public int getOffset(){
+		return this.offset;
+	}
+	
+	public int getFileID(){
+		return this.fileID.getDFileID();
+	}
+	
+	public ArrayList<Integer> getBlockMap(){
+		return this.blockMap;
+	}
+}
