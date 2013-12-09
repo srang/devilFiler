65a8a8083e06ad8faa6214aef557287af81f2456
Brad Levergood <brad.levergood@gmail.com>
2013-12-09 18:02:31 -0500

    modify test files to log output and be run by new universal test program, Main.java

--- src/test/MultiThreadedTest.java
+++ src/test/MultiThreadedTest.java
@@ -1,10 +1,34 @@
 package test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
 import common.DFileID;
 import dfs.LocalDFS;
 
 public class MultiThreadedTest {
-    public static void main(String[] args) throws InterruptedException {
+    public MultiThreadedTest() {
+    
+    }
+    
+    public void run()  throws InterruptedException{
+    	
+		File log = new File("MultiThreadedTest.log");
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(log);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		PrintStream output = System.out;
+		PrintStream ps = new PrintStream(fos); // switch these interchangeably for terminal and log-file output
+		System.setOut(ps);
+    	System.out.println("3. Access DFiler with three client threads, writing 3 strings to three separate files," +
+    			"and then read those out.  Finally, print out all used DFileIDs seen by the DFiler\n");
         LocalDFS dfs = new LocalDFS();
         dfs.init();
         byte[] file1 = "The quick brown fox jumped over the lazy dog".getBytes();
@@ -28,8 +52,18 @@ public class MultiThreadedTest {
         System.out.println(client.read());
         System.out.println(client1.read());
         System.out.println(client2.read());
+        System.out.println("\nDFileIDs in the DFiler:");
         for (DFileID dFileID : dfs.listAllDFiles()) {
             System.out.println(dFileID);
         }
+        
+        ps.close();
+        output.close();
+        try {
+			fos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
